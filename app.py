4b06ca506d9290829268e3ee2be1892985f01e0b
NL647 <76846739+NL647@users.noreply.github.com>
2022-09-10 20:08:53 +0200

    cleaned code

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ import os
 from prettytable import PrettyTable
 from termcolor import colored
 os.system('color')
+import sys
+
+#for encoding € char
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 
-search = input('What product do you search for\n')
+search = input('What product do search for\n')
 sCleaned = search.replace(" ", "+")
 # print(sCleaned)
 
@@ -17,18 +22,19 @@ url = "https://www.etsy.com/fr/search?q={}&explicit=1&order=highest_reviews".for
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'lxml')
 
-# Python code to find frequency of each word
+# Python code to find frequency of each word in the title of products for SEO
 def freq(str):
  
     # break the string into list of words
     str = str.split()        
     str2 = []
-    str3 = ["is", "-", "/", "et","de", "a", "•", '|', "la", "le", "the", "pour", "and", "of", "&", ",", ".", ":" ]
+    #remove those words from occurence count
+    str3 = ["is", "-", "/", "et", "avec","de", "a", "•", '|', "la", "le", "the", "pour", "and", "of", "&", ",", ".","en", ":", "des", "d'", "à", "d’", "les"]
  
     # loop till string values present in list str
     for i in str:            
  
-        # checking for the duplicacy
+        # checking for the duplicate
         if i not in str2 and i not in str3:
  
             # insert value in str2
@@ -38,16 +44,14 @@ def freq(str):
  
         # count the frequency of each word(present
         # in str2) in str and print
-        if str.count(str2[i]) > 5:
+        if str.count(str2[i]) > 2:
             
             
         	print('Frequency of', str2[i], 'is :', str.count(str2[i]))
-         	my_table = PrettyTable()
-			my_table.field_names = ["No.", "Keyword"]
-			my_table.add_row([str.count(str2[i]), str2[i]])
+
          
         
-        
+#Display data in the console        
 for item in soup.select('.v2-listing-card__info'):
 	try:
 		file_object = open('text.txt', 'a')
@@ -58,13 +62,13 @@ for item in soup.select('.v2-listing-card__info'):
 		extra = item.select('.wt-text-caption')[1].get_text().strip()
 		print('----------------------------------------')
 
-		print(colored(f"TITRE: {titre}" , 'white', attrs=['bold', 'blink']))
-		print(colored(f"Prix: {prix}", 'green', attrs=['reverse', 'blink']))
+		print(colored(f"TITRE: {titre}\n" , 'white', attrs=['bold', 'blink']))
+		print(colored(f'Prix en EUROS: {prix}', 'green', attrs=['reverse', 'blink'] , ))
 		print(colored(f"Stars: {stars}" , 'yellow', attrs=['reverse', 'blink']))
-		print(colored(f"Extra Infos: {extra}" , 'blue', attrs=['bold', 'blink']))
+		print(colored(f"Number of Reviews: {extra}" , 'blue', attrs=['bold', 'blink']))
   
 		print('----------------------------------------')
-		# Append at the end of file
+		# Append data to file
 		file_object.write(titre)
 		# Close the file
 		file_object.close()
@@ -78,8 +82,9 @@ for item in soup.select('.v2-listing-card__info'):
 	except Exception as e:
 		raise e
 
-		
+
 with open('text.txt', 'r') as file:
     data = file.read().replace('\n', '')  
-str = data
-freq(str)
+freq(data)
+#delete tmp file
+os.remove("text.txt")
